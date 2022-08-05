20bd37568a411dc1b4f8108370010a8f6e791505
“Diwakar” <diwakarsingh052@gmail.com>
2022-08-05 19:25:55 +0530

    .

--- service-app/auth/auth.go
+++ service-app/auth/auth.go
@@ -12,15 +12,18 @@ type ctxKey int
 // Key is used to store/retrieve a Claims value from a context.Context.
 const Key ctxKey = 1
 
+// Claims is our payload/data for out jwt token
 type Claims struct {
 	jwt.RegisteredClaims
 	Roles []string `json:"roles"`
 }
 
+// Auth struct privateKey field would be used to verify and generate token
 type Auth struct {
-	privateKey *rsa.PrivateKey
+	privateKey *rsa.PrivateKey // this is key we would get after parsing the private.pem file
 }
 
+// NewAuth func set the privateKey in the Auth struct and returns the instance of it to the caller
 func NewAuth(privateKey *rsa.PrivateKey) (*Auth, error) {
 	if privateKey == nil {
 		return nil, errors.New("private key cannot be nil")
@@ -33,8 +36,10 @@ func NewAuth(privateKey *rsa.PrivateKey) (*Auth, error) {
 
 func (a *Auth) GenerateToken(claims Claims) (string, error) {
 
+	//jwt.NewWithClaims takes a signingMethod and claims struct to generate a token on basis of it
 	tkn := jwt.NewWithClaims(jwt.SigningMethodRS256, &claims)
 
+	//signing our token with our private key
 	tokenStr, err := tkn.SignedString(a.privateKey)
 
 	if err != nil {
@@ -49,6 +54,7 @@ func (a *Auth) ValidateToken(tokenStr string) (Claims, error) {
 
 	var claims Claims
 
+	// verifying token with our public key
 	token, err := jwt.ParseWithClaims(tokenStr, &claims, func(token *jwt.Token) (interface{}, error) {
 		return a.privateKey.Public(), nil
 	})
--- service-app/middleware/auth.go
+++ service-app/middleware/auth.go
@@ -12,19 +12,25 @@ import (
 func (m *Mid) Authenticate(next web.HandlerFunc) web.HandlerFunc {
 	return func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 
+		// fetching data from Authorization header
 		authHeader := r.Header.Get("Authorization")
+
+		//token format :- bearer <token> // we trying to separate both strings with space
 		parts := strings.Split(authHeader, " ")
 
+		// checking if we have got exactly to values in auth header and first word is bearer
 		if len(parts) != 2 || strings.ToLower(parts[0]) != "bearer" {
-			err := errors.New("expected authorization header format: bearer <token>")
+			err := errors.New("expected authorization header format: Bearer <token>")
 			return web.NewRequestError(err, http.StatusUnauthorized)
 		}
-		claims, err := m.A.ValidateToken(parts[1])
 
+		// sending the token to the auth layer for verification
+		claims, err := m.A.ValidateToken(parts[1])
 		if err != nil {
 			return web.NewRequestError(err, http.StatusUnauthorized)
 		}
-
+		// putting the token in the context so we can see the values in the claims struct in the request life cycle //
+		// specifically we will look for the subject field as it stores unique user id which will helpful to identify for whom this token was genrated
 		ctx = context.WithValue(ctx, auth.Key, claims)
 
 		return next(ctx, w, r)
